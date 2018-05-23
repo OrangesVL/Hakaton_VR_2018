046567c555a9f738e85225b026b19f6be0538ea3
Vasiliy <kaktusv6@gmail.com>
2018-05-24 03:28:51 +1000

    Добавлены элементы сцены
    Генератор карты. Поля для панелей. UI для конвеера. Реализовано взятие элементов контролерами. Реализовано взаимодействие с UI через лазер контроллера.

--- Assets/Scripts/Block.cs
+++ Assets/Scripts/Block.cs
@@ -5,18 +5,40 @@ using UnityEngine;
 public class Block : MonoBehaviour {
 
 	public bool isAddBox = false;
-
+	public bool isGrab = false;
 
 	VRTK.VRTK_InteractableObject interactableObject;
+	// public bool isGrape;
+
+	Rigidbody rigidbody;
 
 	// Use this for initialization
 	void Start () {
+		rigidbody = GetComponent<Rigidbody>();
 		interactableObject = GetComponent<VRTK.VRTK_InteractableObject>();
+		
+		if (interactableObject == null) {
+			return;
+		}
+
 		interactableObject.InteractableObjectGrabbed += new VRTK.InteractableObjectEventHandler(InteractableObjectGrabbed);	
+		interactableObject.InteractableObjectUngrabbed += new VRTK.InteractableObjectEventHandler(InteractableObjectUngrabbed);
 	}
 
 	public void InteractableObjectGrabbed(object sender, VRTK.InteractableObjectEventArgs e) {
 		Debug.Log("Grabe Object");
+		isGrab = true;
+	}
+
+	public void InteractableObjectUngrabbed(object sender, VRTK.InteractableObjectEventArgs e) {
+		Debug.Log("UnGrabe Object");
+		isGrab = false;
+		if (rigidbody == null) {
+			return;
+		}
+
+		rigidbody.useGravity = true;
+		rigidbody.isKinematic = false;
 	}
 	
 	// Update is called once per frame
